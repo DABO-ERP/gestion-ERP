ee7c43edb07e49f94b8f5cdb241240566c41d3c4
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2026-01-27 21:18:22 -0500

    update: first commit

---
+++ src/main/java/org/uq/gestionerp/GestionErpApplication.java
@@ -0,0 +1,13 @@
+package org.uq.gestionerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/uq/gestionerp/GestionErpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uq.gestionerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
